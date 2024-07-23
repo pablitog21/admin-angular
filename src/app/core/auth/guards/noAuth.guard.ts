333f9423ae707049785dcebf33075a780a2a52ae
pablitog21 <149818575+pablitog21@users.noreply.github.com>
2024-07-23 13:18:06 -0500

    Update noAuth.guard.ts

--- src/app/core/auth/guards/noAuth.guard.ts
+++ src/app/core/auth/guards/noAuth.guard.ts
@@ -1,27 +1,53 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, take, tap } from 'rxjs/operators';
-import { AuthService } from '../auth.service';
+import { CanMatch, Route, Router, UrlSegment, UrlTree } from '@angular/router';
+import { Observable, of, switchMap } from 'rxjs';
+import { AuthService } from 'app/core/auth/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class NotAuthGuard implements CanActivate {
+export class NoAuthGuard implements CanMatch
+{
+    /**
+     * Constructor
+     */
+    constructor(
+        private _authService: AuthService,
+        private _router: Router
+    )
+    {
+    }
 
-  constructor(private authService: AuthService, private router: Router) {}
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
 
-  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-    // Verifica si el usuario está autenticado
-    return this.authService.isAuthenticated$.pipe(
-      take(1), // Toma el primer valor emitido y completa el observable
-      map(isAuthenticated => !isAuthenticated), // Invierte el valor: si está autenticado, devuelve false
-      tap(isNotAuthenticated => {
-        if (!isNotAuthenticated) {
-          // Si el usuario está autenticado, redirige a una ruta alternativa
-          this.router.navigate(['/dashboard']); // Cambia '/dashboard' según tu aplicación
-        }
-      })
-    );
-  }
+    /**
+     * Can match
+     *
+     * @param route
+     * @param segments
+     */
+    canMatch(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
+    {
+        return this._check();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Private methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Check the authenticated status
+     *
+     * @private
+     */
+    private _check(): Observable<boolean>
+    {
+        // Check the authentication status and return an observable of
+        // "true" or "false" to allow or prevent the access
+        return this._authService.check().pipe(
+            switchMap((authenticated) => of(!authenticated))
+        );
+    }
 }
