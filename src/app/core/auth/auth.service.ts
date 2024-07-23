ebb5bfec41efa467083958df3ceec38dec0bb512
pablitog21 <149818575+pablitog21@users.noreply.github.com>
2024-07-23 13:22:38 -0500

    Update auth.service.ts

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -33,5 +33,60 @@ signIn(user: User): Observable<any> {
         })
       )
   }
+
+  signInUsingToken(): Observable<any> {
+        // Sign in using the token
+        return this._httpClient.post('api/auth/sign-in-with-token', {
+            accessToken: this.accessToken
+        }).pipe(
+            catchError(() =>
+
+                // Return false
+                of(false)
+            ),
+            switchMap((response: any) => {
+
+                // Replace the access token with the new one if it's available on
+                // the response object.
+                //
+                // This is an added optional step for better security. Once you sign
+                // in using the token, you should generate a new one on the server
+                // side and attach it to the response object. Then the following
+                // piece of code can replace the token with the refreshed one.
+                if (response.accessToken) {
+                    this.accessToken = response.accessToken;
+                }
+
+                // Set the authenticated flag to true
+                this._authenticated = true;
+
+                // Store the user on the user service
+                this._userService.user = response.user;
+
+                // Return true
+                return of(true);
+            })
+        );
+    }
+
+   check(): Observable<boolean> {
+        // Check if the user is logged in
+        if (this._authenticated) {
+            return of(true);
+        }
+
+        // Check the access token availability
+        if (!this.accessToken) {
+            return of(false);
+        }
+
+        // Check the access token expire date
+        if (AuthUtils.isTokenExpired(this.accessToken)) {
+            return of(false);
+        }
+
+        // If the access token exists and it didn't expire, sign in using it
+        return this.signInUsingToken();
+    }
   
 }
